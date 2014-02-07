22c48f8a0bf3c81654ba0fd8872ed9f35e28f257
cmyr <colin@cmyr.net>
2014-02-07 19:33:38 +0200

    rhymes_for_word(), fixed batch phoneme extraction
    - added a function for finding rhymes amongst a list of words;
    - fixed batch extraction of phonemes
    - added a global debug variable to rhyme.py

--- poetryutils2/rhyme.py
+++ poetryutils2/rhyme.py
@@ -5,8 +5,11 @@ from __future__ import unicode_literals
 
 import re
 import os
+import sys
 import anydbm
 import cPickle as pickle
+import time
+import multiprocessing
 
 from collections import defaultdict
 from mpycache import LRUCache
@@ -14,16 +17,14 @@ from mpycache import LRUCache
 import utils
 
 
-
 # PHONEME_INDEX_PICKLE_PATH = os.path.join(utils.MODULE_PATH, "data/phonemes.p")
-
 # phoneme_index = None
 # modified_phonemes = None
+# wordlist = None
+# rhyme_table = None
+_RHYME_DEBUG = True
 
-wordlist =  None
-rhyme_table = None
-
-double_end_letters = set(['f','e','l','i','o','s'])
+double_end_letters = set(['f', 'e', 'l', 'i', 'o', 's'])
 ipa_vowels = set("ˈˌaeiouyɑɛɪöɩɔɚɷʊʌœöøəæː")
 
 
@@ -33,25 +34,20 @@ ipa_vowels = set("ˈˌaeiouyɑɛɪöɩɔɚɷʊʌœöøəæː")
 #         print('loading phoneme index')
 #         try:
 #             phoneme_index = pickle.load(open(PHONEME_INDEX_PICKLE_PATH, 'r'))
-#             # modified_phonemes = convert_phoneme_index()
-#             # wordlist = phoneme_index.keys()
+# modified_phonemes = convert_phoneme_index()
+# wordlist = phoneme_index.keys()
 #         except IOError:
 #             print('io error in rhymes')
 #             phoneme_index = dict()
 
 
-
 # def build_rhyme_index():
 #     global rhyme_table
 #     rhyme_table = defaultdict(set)
-
 #     for w,p in phoneme_index.items():
 #         e = end_sound(p)
 #         rhyme_table[e].add(w)
-
-
 # def get_phonemes(word):
-    
 #     if not phoneme_index:
 #         load_phoneme_index()
 #     word = normalize_word(word)
@@ -60,57 +56,27 @@ ipa_vowels = set("ˈˌaeiouyɑɛɪöɩɔɚɷʊʌœöøəæː")
 #         if not phonemes:
 #             phonemes = _extract_phonemes(word)[1]
 #         phoneme_index[word] = phonemes
-
 #     return phonemes
-     
-
 dbpath = os.path.join(utils.MODULE_PATH, 'data/phonemes.db')
 db = None
+stats = dict()
 phone_cache = LRUCache()
+
 
 def open_db():
     global db
     db = anydbm.open(dbpath, 'cs')
+    stats['new'] = 0
+
 
 def close_db():
-    db.close() 
+    if stats['new'] > 0:
+        print('added %d new words to phoneme index' % stats['new'])
+    db.close()
 
-def _get_phonemes(word):
-    # word = normalize_word(word)
-    assert isinstance(word, basestring) 
-    phonemes = phone_cache.get(word)
-
-    if not phonemes:
-        k = word.encode('utf8')
-        try:
-            # dbm only takes string keys/values
-            phonemes = db[k].decode('utf8')
-            phonemes = _adjust_phonemes(phonemes)
-            phone_cache.put(word, phonemes)
-            return phonemes
-        except KeyError:
-            phonemes = _extract_phonemes(word)[1]
-            db[k] = phonemes.encode('utf8')
-            phonemes = _adjust_phonemes(phonemes)
-            phone_cache.put(word, phonemes)
-
-    return phonemes
-
-def _adjust_phonemes(phonemes):
-    """
-    some adjustments we make to ipa 
-    """
-    
-    if not isinstance(phonemes, basestring):
-        print('bad phoneme data:', type(phonemes))
-        return
-    phonemes = re.sub(r'[ˈˌ]', '', phonemes, flags=re.UNICODE)
-    phonemes = re.sub(r'(oː|ɔː)', 'ö', phonemes, flags=re.UNICODE)
-
-    return phonemes
 
 def rhyme_word(text, debug=False):
-    #if the last word isn't a word, we could just pass
+    # if the last word isn't a word, we could just pass
     words = text.split()
     word = None
 
@@ -129,7 +95,6 @@ def rhyme_word(text, debug=False):
         return None
 
     return normalize_word(word)
-
 
 
 def _normalize_word(word):
@@ -155,8 +120,47 @@ def _normalize_word(word):
                 repl = word[-1]
                 word = re.sub(pattern, repl, word)
 
-
     return word
+
+
+def get_phonemes(word):
+    if _RHYME_DEBUG:
+        assert word == _normalize_word(word), print(
+            'passed unnormalized %s to get_phonemes' % word)
+
+    assert isinstance(word, basestring)
+    phonemes = phone_cache.get(word)
+
+    if not phonemes:
+        k = word.encode('utf8')
+        try:
+            # dbm only takes string keys/values
+            phonemes = db[k].decode('utf8')
+            phonemes = _adjust_phonemes(phonemes)
+            phone_cache.put(word, phonemes)
+            return phonemes
+        except KeyError:
+            phonemes = _extract_phonemes(word)[1]
+            db[k] = phonemes.encode('utf8')
+            phonemes = _adjust_phonemes(phonemes)
+            phone_cache.put(word, phonemes)
+
+    return phonemes
+
+
+def _adjust_phonemes(phonemes):
+    """
+    some adjustments we make to ipa 
+    """
+
+    if not isinstance(phonemes, basestring):
+        print('bad phoneme data:', type(phonemes))
+        return
+    phonemes = re.sub(r'[ˈˌ]', '', phonemes, flags=re.UNICODE)
+    phonemes = re.sub(r'(oː|ɔː)', 'ö', phonemes, flags=re.UNICODE)
+
+    return phonemes
+
 
 def _end_sound(phonemes):
     if not phonemes:
@@ -180,51 +184,67 @@ def _end_sound(phonemes):
     return sound
 
 
-def add_new_words(wordlist):
+def sound_for_word(word, func=_end_sound):
     """
-    add new words to our lookup table
+    a sort of convenience function for getting a specific
+    sound for a word, returning None on any errors
     """
-    num_words = len(wordlist)
-    print('extracting phonemes for %d new words' % num_words)
-    start = time.time()
-    pool = multiprocessing.Pool(4)
-    result = pool.map_async(_extract_phonemes, words)
-    
-    while True:
-        if result.read():
-            break
-        status = "%d/%d\r" % (num_words, result._number_left)
-        sys.stdout.write(status)
-        sys.stdout.flush()
-    
-    result = result.get()
-    # for w,p in result:
-    #     phoneme_index[w] = p
-    #     modified_phonemes.add((w, adjust_phonemes(p)))
-    print('finished in %0.2f' % (time.time() - start))
-    print('saving updated phoneme list')
-    # pickle.dump(phoneme_index, open(PHONEME_INDEX_PICKLE_PATH, 'w'))
+    try:
+        w = _normalize_word(word)
+        p = get_phonemes(w)
+        sound = func(p)
+        return sound
+    except ValueError as err:
+        return None
 
 
 def _extract_phonemes(word):
-    espeak_output = os.popen3("speak -v english-us -q --ipa %s"%word, 'r')[1].read()
+    espeak_output = os.popen3("speak -v english-us -q --ipa %s" %
+                              word, 'r')[1].read()
     phonemes = espeak_output.strip().decode('utf8')
+    stats['new'] += 1
     return word, phonemes
+
 
 def words_are_homophony(w1, w2):
     return False
+
 
 def words_rhyme(w1, w2):
     w1 = _normalize_word(w1)
     w2 = _normalize_word(w2)
 
-    p1 = _get_phonemes(w1)
-    p2 = _get_phonemes(w2)
+    p1 = get_phonemes(w1)
+    p2 = get_phonemes(w2)
     if _end_sound(p1) == _end_sound(p2):
         if not words_are_homophony(w1, w2):
             return True
 
     return False
+
+
+def rhymes_for_word(word, wordlist):
+    open_db()
+    new_words = set(w for w in wordlist if w.encode('utf8') not in db)
+    
+    if len(new_words):
+        close_db()
+        # because add_new opens it itself
+        add_new_words(new_words)
+        open_db()
+
+    print('looking for rhymes of %s amongst %d words' % (word, len(wordlist)))
+    try:
+        sound = sound_for_word(word)
+        if not sound:
+            print('could not extract sound for word: %s' % word)
+
+        rhymes = [w for w in wordlist if sound_for_word(w) == sound]
+    finally:
+        close_db()
+    return rhymes
+
+
 
 def rhymes_for_lines(lines, textkey=None):
     """ 
@@ -240,12 +260,11 @@ def rhymes_for_lines(lines, textkey=None):
         try:
             w = rhyme_word(l)
             if w:
-                p = _get_phonemes(w)
+                p = get_phonemes(w)
                 e = _end_sound(p)
                 organized_rhyme[e].append((l, p))
         except ValueError:
             fails += 1
-
 
     print('failed to find rhyme words in %d lines' % fails)
 
@@ -269,18 +288,38 @@ def _sort_rhymes(rhymes):
     return sorted_rhymes.values()
 
 
+def add_new_words(wordlist):
+    """
+    add new words to our lookup table
+    """
+    open_db()
+    num_words = len(wordlist)
+    print('extracting phonemes for %d new words' % num_words)
+    start = time.time()
+    pool = multiprocessing.Pool(4)
+    result = pool.map_async(_extract_phonemes, wordlist)
 
+    while True:
+        if result.ready():
+            break
+        status = "%d/%d\r" % (num_words, result._number_left)
+        sys.stdout.write(status)
+        sys.stdout.flush()
+        time.sleep(1)
 
-
-
-
-
+    result = result.get()
+    for w,p in result:
+        db[w.encode('utf8')] = p.encode('utf8')
+    #     modified_phonemes.add((w, adjust_phonemes(p)))
+    print('finished in %0.2f' % (time.time() - start))
+    close_db()
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('arg1', type=str, help="required argument")
-    parser.add_argument('arg2', '--argument-2', help='optional boolean argument', action="store_true")
+    parser.add_argument('arg2', '--argument-2',
+                        help='optional boolean argument', action="store_true")
     args = parser.parse_args()
 
 
