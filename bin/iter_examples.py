c18e92afe43dc4bb81d1d79321ea31549ddefa51
cmyr <colin@cmyr.net>
2014-05-11 14:32:33 -0400

    added example limerick iter

--- bin/iter_examples.py
+++ bin/iter_examples.py
@@ -14,9 +14,28 @@ def example_haiku_iter():
                 haiku = list()
 
 
+def example_limerick_iter():
+    source = '/Users/cmyr/Dev/projects/poetryutils2/sources/maylimericks.txt'
+    with open(source) as f:
+        limerick = list()
+        for line in f:
+            if len(line.rstrip()):
+                limerick.append(line)
+            if len(limerick) == 5:
+                yield tuple(limerick)
+                limerick = list()
+
+
+
 def test_haiku():
     for poem in example_haiku_iter():
         print(poem)
 
+def test_limericks():
+    for poem in example_limerick_iter():
+        print(poem)
+
+
 if __name__ == "__main__":
     test_haiku()
+    test_limericks()
