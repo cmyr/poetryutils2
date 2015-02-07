0b62d1e1d83381fae71c1b6c221a42ee237703e2
cmyr <colin@cmyr.net>
2015-02-07 10:44:56 -0500

    updated init

--- poetryutils2/__init__.py
+++ poetryutils2/__init__.py
@@ -1,6 +1,6 @@
 
 from .syllables import count_syllables
-from .utils import line_iter, filter_line
-from .sorting import Coupler, Rhymer, Haikuer
+from .utils import line_iter, filter_line, prune_dict
+from .sorting import Coupler, Rhymer, Haikuer, Limericker
 import filters
 
