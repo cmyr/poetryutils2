e1c466d42c3397cecaabedffb8e022ae6d5ca23a
cmyr <colin@cmyr.net>
2016-04-01 20:50:13 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='poetryutils2',
+      version='0.1',
+      description='poetry writing tools',
+      author='Colin Rofls',
+      author_email='colin@cmyr.net',
+      url='https://github.com/cmyr/poetryutils2',
+      packages=['poetryutils2']
+      )
